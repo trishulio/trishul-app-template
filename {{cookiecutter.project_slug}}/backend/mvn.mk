04cc4adb669ea0f052c099f1e4f72e3fdbc81cd9
Rishab Manocha <rishab.manocha@outlook.com>
2026-05-02 07:21:36 -0700

    Updated mvn commands to use more threads and deleted unused config enable_formatting

--- {{cookiecutter.project_slug}}/backend/mvn.mk
+++ {{cookiecutter.project_slug}}/backend/mvn.mk
@@ -1,12 +1,13 @@
 .PHONY: install compile
 
 MAVEN := docker-compose --env-file mvn.env -f docker-compose-bin.yml run --rm --remove-orphans mvn
+THREADS ?= 2C
 
 install:
-	$(MAVEN) mvn clean install
+	$(MAVEN) mvn clean install -T $(THREADS)
 
 compile:
-	$(MAVEN) mvn compile -T 1C -q
+	$(MAVEN) mvn compile -q -T $(THREADS)
 
 dependency_tree:
 	$(MAVEN) mvn dependency:tree
