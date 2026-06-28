71329a350214de735eb00437ef60afddb46c30d1
Rishab Manocha <rishab.manocha@outlook.com>
2026-06-28 15:17:07 -0700

    Added custom maven command

--- {{cookiecutter.project_slug}}/backend/mvn.mk
+++ {{cookiecutter.project_slug}}/backend/mvn.mk
@@ -3,6 +3,15 @@
 DOCKER_COMPOSE_MAVEN := docker-compose --env-file mvn.env -f docker-compose-bin.yml run --rm --remove-orphans mvn
 THREADS ?= 2C
 
+ifeq ($(firstword $(MAKECMDGOALS)),maven)
+  MAVEN_ARGS := $(wordlist 2,$(words $(MAKECMDGOALS)),$(MAKECMDGOALS))
+  # Turn the arguments into do-nothing targets so Make doesn't complain
+  $(eval $(MAVEN_ARGS):;@:)
+endif
+
+maven:
+	$(DOCKER_COMPOSE_MAVEN) mvn $(MAVEN_ARGS)
+
 install:
 	$(DOCKER_COMPOSE_MAVEN) mvn clean install -T $(THREADS)
 
