6f0f72d77f6a2ef63f572e29d1245145bf805d85
Rishab Manocha <rishab.manocha@outlook.com>
2026-07-01 17:30:28 -0700

    Updated goal of generate-collection to bruno-collection

--- {{cookiecutter.project_slug}}/backend/bruno.mk
+++ {{cookiecutter.project_slug}}/backend/bruno.mk
@@ -1,8 +1,8 @@
-.PHONY: generate-collection
+.PHONY: bruno-collection
 
 DOCKER_COMPOSE_BRUNO := docker-compose --env-file bruno.env -f bruno/docker-compose.yml run --rm bruno
 
-generate-collection:
+bruno-collection:
 	rm -rf bruno-collection
 	$(DOCKER_COMPOSE_BRUNO) bru import openapi -s api/openapi.json -o bruno-collection -n "{{cookiecutter.project_name}} API" --collection-format bru
 	$(DOCKER_COMPOSE_BRUNO) node bruno/scripts/bruno-post-process.js
