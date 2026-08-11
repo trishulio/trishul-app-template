303f039eda59ad7dcc9cef7ca63ac676be5634e5
Rishab Manocha <rishab.manocha@outlook.com>
2026-08-11 13:16:30 -0700

    Update helm deployment configs and build env vars

--- {{cookiecutter.project_slug}}/backend/helm.mk
+++ {{cookiecutter.project_slug}}/backend/helm.mk
@@ -1,11 +1,15 @@
-.PHONY: install containerize login_repo publish deploy undeploy deploy containerize_local set_credentials
+.PHONY: deploy undeploy
 
-APP_NAME:={{cookiecutter.project_slug}}-backend
-VERSION:=1.0.0-SNAPSHOT
-VALUES_FILE:=values-development.yml
-NAMESPACE:=local
+APP_NAME:={{ cookiecutter.project_slug }}-service
+VERSION:=1.0.20-SNAPSHOT
+VALUES_FILE:=values-$(ENV_NAME).yml
+APP_NAME_PREFIX:=$(shell awk '/^app:/ {flag=1; next} /^[^ ]/ {flag=0} flag && /^  name:/ {print $$2}' chart/values.yaml | sed 's/-service//')
+ENV_NAME:=$(shell awk '/^env:/ {print $$2}' chart/$(VALUES_FILE))
+NAMESPACE:=$(APP_NAME_PREFIX)-$(ENV_NAME)
 
-DOCKER_COMPOSE_HELM := docker-compose --env-file helm.env -f docker-compose-bin.yml run --rm --remove-orphans helm
+## TODO: Change this to deployment and calculate values-file, and namespace from it.
+
+DOCKER_COMPOSE_HELM := docker-compose -f ../docker-compose-bin.yml run --rm --remove-orphans helm
 
 deploy:
 	(cd chart && ${DOCKER_COMPOSE_HELM} upgrade --install -f values.yaml -f ${VALUES_FILE} -n ${NAMESPACE} ${APP_NAME} . --set image.tag=${VERSION})
