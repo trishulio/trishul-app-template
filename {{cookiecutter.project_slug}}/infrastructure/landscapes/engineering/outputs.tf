a2f51619d448884a928daefeacba9989de47fd7c
Rishab Manocha <rishab.manocha@outlook.com>
2026-09-04 10:22:55 +0530

    Added sensitivity to true

--- {{cookiecutter.project_slug}}/infrastructure/landscapes/engineering/outputs.tf
+++ {{cookiecutter.project_slug}}/infrastructure/landscapes/engineering/outputs.tf
@@ -1,61 +1,62 @@
 output "aws_cognito_user_pool_id" {
-    value = module.landscape_engineering.aws_cognito_user_pool_id
+  value = module.landscape_engineering.aws_cognito_user_pool_id
 }
 
 output "aws_cognito_identity_pool_id" {
-    value = module.landscape_engineering.aws_cognito_identity_pool_id
+  value = module.landscape_engineering.aws_cognito_identity_pool_id
 }
 
 output "aws_cognito_access_key_id" {
-    value = module.landscape_engineering.aws_cognito_access_key_id
-    sensitive = true
+  value     = module.landscape_engineering.aws_cognito_access_key_id
+  sensitive = true
 }
 
 output "aws_cognito_access_secret_key" {
-    value = module.landscape_engineering.aws_cognito_access_secret_key
-    sensitive = true
+  value     = module.landscape_engineering.aws_cognito_access_secret_key
+  sensitive = true
 }
 
 output "aws_iam_access_key" {
-    value = module.landscape_engineering.aws_iam_access_key
-    sensitive = true
+  value     = module.landscape_engineering.aws_iam_access_key
+  sensitive = true
 }
 
 output "aws_iam_access_secret_key" {
-    value = module.landscape_engineering.aws_iam_access_secret_key
-    sensitive = true
+  value     = module.landscape_engineering.aws_iam_access_secret_key
+  sensitive = true
 }
 
 output "aws_s3_access_key_id" {
-    value = module.landscape_engineering.aws_s3_access_key_id
-    sensitive = true
+  value     = module.landscape_engineering.aws_s3_access_key_id
+  sensitive = true
 }
 
 output "aws_s3_access_secret_key" {
-    value = module.landscape_engineering.aws_s3_access_secret_key
-    sensitive = true
+  value     = module.landscape_engineering.aws_s3_access_secret_key
+  sensitive = true
 }
 
 output "aws_secrets_manager_access_key_id" {
-    value = module.landscape_engineering.aws_secrets_manager_access_key_id
-    sensitive = true
+  value     = module.landscape_engineering.aws_secrets_manager_access_key_id
+  sensitive = true
 }
 
 output "aws_secrets_manager_access_secret_key" {
-    value = module.landscape_engineering.aws_secrets_manager_access_secret_key
-    sensitive = true
+  value     = module.landscape_engineering.aws_secrets_manager_access_secret_key
+  sensitive = true
 }
 
 output "tenant_admin_group_id" {
-    value = module.landscape_engineering.tenant_admin_group_id
+  value = module.landscape_engineering.tenant_admin_group_id
 }
 
 output "sentry_dsn" {
-    value       = module.landscape_engineering.sentry_dsn
-    description = "The public DSN for the Sentry project"
+  value       = module.landscape_engineering.sentry_dsn
+  description = "The public DSN for the Sentry project"
+  sensitive   = true
 }
 
 output "openrouter_api_key" {
-    value     = module.landscape_engineering.openrouter_api_key
-    sensitive = true
+  value     = module.landscape_engineering.openrouter_api_key
+  sensitive = true
 }
